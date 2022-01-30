a8f2cceffb0ab0d4b5bb849ed493d090895da5c8
CosteRaduRares <79933436+CosteRaduRares@users.noreply.github.com>
2022-01-30 23:16:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,110 +1,114 @@
 //GLOBAL
-const countryList = document.querySelector('.country')
-const countryDetails = document.querySelector('.details-country')
-const search = document.querySelector('.search')
-const NoSearch = document.querySelector('.nsearch')
-const topBtn = document.getElementById('.topbtn')
-const filterBtn = document.getElementById('.filter')
-const modalBtn = document.getElementById('.modal-close-btn')
-const buttons = document.querySelector('.buttons')
+const countryList = document.querySelector('.country');
+const countryDetails = document.querySelector('.country-details-content');
+const search = document.querySelector('.search');
+const NoSearch = document.querySelector('.nsearch');
+const topBtn = document.getElementById('#topbtn');
+const filterBtn = document.getElementById('filter');
+const modalBtn = document.getElementById('modal-close-btn');
+const buttons = document.querySelector('.buttons');
 
 countryList.addEventListener('click', getModalInfo)
     filterBtn.addEventListener('click', () => {
-    filterBtn.classList.toggle(open)
-})
+    filterBtn.classList.toggle('open')
+});
 modalBtn.addEventListener('click', () => {
     countryDetails.parentElement.classList.remove('showDetails')
-})
+});
+
 // Create one Country
  const createCountry = ({
-     name,capital,region,population,flag})=>{
-         const formattedCapital = capital.lenght > 0 ? `${capital}` : "-"
-         const formattedRegion = region.lenght > 0 ? `${region}` : "-"
-         return `<div class="country">
-             <img src="${flag}" alt="flag"></img>
-             <div class="info" id="country" data-value="${name}">
-                 <h3 class="country-name">${name}</h3>
-                 <p>Capital : ${formattedCapital}</p>
-                 <p>Region : ${formattedRegion}</p>
-                 <p>Population : ${population.toLocaleString()}</p>
-                 <a href="#" class="country-button">More!</a>
-             </div>
-         </div>`
-        }
+      name,
+      capital, 
+      region, 
+      population, 
+      flags
+    }) => {
+        return      `<div class="countries-wrapper"> 
+                    <img src="${flags.png}" alt="Flags" />
+                    <div class="country-info" id="country" data-value="${name.common}">
+                    <h3 class="country-name">${name.common}</h3>
+                    <p>Capital: ${capital}</span>.</p>
+                    <p>Region: ${region}</p>
+                    <p>Population: ${population.toLocaleString()} </p>
+                    <a href="#" class="country-btn">See More</a>
+                     </div>
+                 </div>`
+        };
 //Search input filter 
-const filterCountries = (arr, search) =>{
-    search = search.toLowerCase()
-    const filteredCountry = arr.filter(country => {
+const filterCountries = (arr,search) =>{
+        search = search
+        const filteredCountries = arr (country => {
         const{
-        name,capital,alpha3Code} = country 
-        const isName = name.toLowerCase().includes(search)
-        const isCapital = name.toLowerCase().includes(search)
-        const isAplha3Code = alpha3Code.toLowerCase().includes(search)
-        return isName || isCapital || isAplha3Code
-    })
-        return filterCountries
-}
+        name, capital, alpha2Code} = country 
+        const iName = name.toLowerCase().includes(search)
+        const iCapital = capital.toLowerCase().includes(search)
+        const iAplha2Code = alpha2Code.toLowerCase().includes(search)
+        return iName || iCapital || iAplha2Code
+    });
+        return filteredCountries;
+};
+     
+//Countries in div
+const renderCountries = arr =>{
+    let contents =""
+    arr.forEach(country =>(contents += createCountry(country)))
+    countryList.innerHTML = contents
+};
 // Sorting Countries
 const sortCountries = (arr, type) =>{
     const countries =[...arr]
     const sortedCountries = countries.sort((a, b) => {
         if(a[type] > b[type]) return 1
         if(a[type] < b[type]) return -1
-        return 0
-    })
-        return sortedCountries
-}
-//Countries in div
-const renderCountries = arr =>{
-    let contents =""
-    arr.forEach(country =>(contents += createCountry(country)))
-    countryList.innerHtml = contents
-}
+        return 0;
+    });
+        return sortedCountries;
+};
 //Information for the modal
 function getModalInfo(e){
     e.preventDefault()
-    if(e.target.classList.contains("country-button")){
-        let countryItem = e.target.parentElement
-        fetch(`https://restcountries.com/v3.1/name/${countryItem.dataset.value}`)
+    if(e.target.classList.contains("country-btn")){
+        countryItem = e.target.parentElement
+        fetch(`https://restcountries.com/v3.1/name/name`)
         .then(Response => Response.json())
         .then(country =>{
             country = country[0]
-            countryModal(country)
+            // countryModal(country)
             countryNeighbours(country)
         })
         .catch(err => console.log("Error :", err))
-    }
-}
+    };
+};
 //Click on Neighbours
 function countryNeighbours(country){
     html = 'Neighbours : '
-    const neighbours = document.querySelector('.neighbours')
-    for(let i = 0; i < country.borders.lenght;i++){
-        html += `<a href="#" class="neighbours-btn">${country.borders[i]}</a>`
-    }
-    neighbours.innerHTML = html
-    const neighboursBtn = document.querySelectorAll('.neighbours-btn')
+    const neighbours = document.querySelector('.neighbour')
+    for(let i = 0; i < country.borders.length;i++){
+        html += `<a href="#" class="neighbour-btn">${country.borders[i]}</a>`
+    };
+     neighbours.innerHTML = html
+    const neighboursBtn = document.querySelectorAll('.neighbour-btn')
     neighboursBtn.forEach(btn =>{
         btn.addEventListener('click', getModalByCode)
-    })
-}
+    });
+};
 //Neighbours Code
 function getModalByCode(e){
     e.preventDefault()
-    let code = e.target.text
-    fetch=('https://restcountries.eu/rest/v3.1/alpha/${code}')
-    .then(Response => Response,json())
+    fetch=('https://restcountries.com/v3.1/alpha/${cca2}')
+    .then(Response => Response.json())
     .then(code =>{
         countryModal(code)
         countryNeighbours(code)
-    })
-}
+    });
+};
 //Modal
 function countryModal(country){
     let current = new Date()
-    let html = `<img src= "${country.flag}" alt="" width="300px">
-                <div>Country : ${country.name}</span></div>
-                <div id="code" data-id=${country.alpha3Code} >Alpha3Code : ${country.alpha3Code}</div>
+      html = `<img src= "${country.flags.svg}" alt="" width="300px">
+                <div>Country : ${country.name.common}</span></div>
+                <div id="code" data-id=${country.cca2} >Alpha2Code : ${country.cca2}</div>
                 <div>Capital : ${country.capital}</div>
                 <div>Region : ${country.region}</div>
                 <div>Population : ${country.population.toLocaleString()}</div>
@@ -113,12 +117,14 @@ function countryModal(country){
                 <div>Timezone : ${country.timezones.join(', ')}</div>
                 <div>Current Time : ${current.toLocaleTimeString()} </div>
                 <div class="neighbour">Neighbours: </div> 
-                <div>Currencies : ${country.currencies.filter(c => c.name).map(c => `${c.name} (${c.code})`).join(", ")}</div>
-                <div>Official languages : ${country.languages.map(language => language.name).join(", ")}
-                </div>`
-        countryDetails.innerHTML
-        countryDetails.parentElement.classList.add('ShowDetails')
-}
+                <div>Currency : ${country.currency}div>
+                <div>Official languages : ${country.languages}</div>`
+        countryDetails.innerHTML;
+        countryDetails.parentElement.classList.add('ShowDetails');
+        
+
+};
+
 //Fetch Country
 fetch('https://restcountries.com/v3.1/all')
     .then(Response => Response.json())
@@ -126,47 +132,46 @@ fetch('https://restcountries.com/v3.1/all')
         renderCountries(sortCountries(countries, 'name'))
         buttons.addEventListener('click', e =>{
             let type = e.target.className
-            if (type === "name"){
+            if (type === 'name'){
                 sortedCountries = sortCountries(countries, type)
                 renderCountries(sortedCountries)
-            } else if(type === "region"){
+            } else if(type === 'region'){
                 sortedCountries = sortCountries(countries, type)
                 renderCountries(sortedCountries)
             }else if(type === 'population'){
                 sortedCountries = sortCountries(countries, type)
                 renderCountries(sortedCountries)
             }else if(type === 'languages'){
-                sortedCountries = countries.sort(function(a, b){
-                    return a.language[0].name.localeCompare(b.language[0].name)
-                })
+                sortedCountries = sortCountries(countries, type)
                 renderCountries(sortedCountries)
             }else if(type === 'timezones'){
                 sortedCountries = sortCountries(countries, type)
                 renderCountries(sortedCountries)
-            }else if(type === 'currenciese'){
-                sortedCountries = countries.sort(function(a, b){
-                    return a.currencies[0].name.localeCompare(b.currencies[0].name)
-                })
-                renderCountries(sortedCountries)
+            }else if(type === 'currency'){
+                sortedCountries = countries.sort(function (a, b) {
+                    return a.currency[0].name.localeCompare(b.currency[0].name)
+                  })
+                  renderCountries(sortedCountries)
             }else{
-                sortedCountries = renderCountries(sortCountries(countries, 'name'))
+                sortedCountries = renderCountries(sortCountries(countries, 'name'));
             }
         })
-        search.addEventListener('input', e =>{
+     
+        search.addEventListener("input", e =>{
             let searchTerm = e.target.value
-            renderCountries(filterCountries(countries, searchTerm))
-            let html = `<div class='nsearch'>No result found</div>`
-            const ifnsearch = filterCountries(countries, searchTerm).lenght < 1 ?
+            renderCountries(filterCountries(countries, searchTerm));
+             html = `<div class="nsearch">No result found</div>`
+            const NoSearch = filterCountries(country, searchTerm).length < 1 ?
             NoSearch.classList.add('showDetails'):
             NoSearch.classList.remove('showDetails')
             
-            if (searchTerm === ''){
-                renderCountries(sortCountries(countries, 'name'))
+            if (searchTerm === ' '){
+                renderCountries(sortCountries(countries, 'name'));
             }
-        })
-    })
-
+        }); 
+    });
     function topFunction(){
         document.body.scrollTop = 0
         document.documentElement.scrollTop = 0
-    }
+    };
+    
